3a83acaacc761e33a8ae0fd04827c5805d105b34
Adam Dehnel <arsdehnel@gmail.com>
2026-09-22 11:17:07 -0500

    chore: knip entry for playwright gallery

--- knip.ts
+++ knip.ts
@@ -2,7 +2,7 @@ export default {
 	tags: ['-lintignore', '-knipTestExport'],
 	ignoreDependencies: ['cloudflare'],
 	ignoreExportsUsedInFile: true,
-	entry: ['tests/mocks/**'],
+	entry: ['tests/mocks/**', 'playwright/gallery/main.tsx'],
 	ignoreFiles: ['src/client.tsx', 'release.config.js'],
 	compilers: {
 		css: (text: string): string => [...text.matchAll(/(?<=@)import[^;]+/g)].join('\n'),
