25eb76d3372f6b7086ccf7a5a8fb816ad640319d
Adam Dehnel <arsdehnel@gmail.com>
2026-09-22 19:19:50 -0500

    fix: move from branch protection to branch rulesets

---
+++ terraform/rulesets-branches.tf
@@ -0,0 +1,31 @@
+resource "github_repository_ruleset" "main" {
+  name        = "main"
+  repository  = github_repository.repo.name
+  target      = "branch"
+  enforcement = "active"
+
+  conditions {
+    ref_name {
+      include = ["refs/heads/main"]
+      exclude = []
+    }
+  }
+
+  rules {
+    required_status_checks {
+      required_check {
+        context = "commitlint / commitlint"
+      }
+      required_check {
+        context = "unit-tests"
+      }
+      required_check {
+        context = "build"
+      }
+      required_check {
+        context = "component-tests"
+      }
+      strict_required_status_checks_policy = true
+    }
+  }
+}
